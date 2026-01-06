a0c86248a9a03cb153ce16dd8100b9aac4bd7cb1
Anton Stafeyev <anton.stafeyev@gmail.com>
2026-01-06 14:58:04 +0500

    yolo

--- Telebot.Playground/Program.cs
+++ Telebot.Playground/Program.cs
@@ -1,7 +1,7 @@
 ﻿
 using Telebot;
 
-var tg = new Telegram("8235635546:AAHnLBwJtsGJYFe11oDvajUoxlocCo-wY54");
+var tg = new Telegram("<YOUR TOKEN>");
 
 // Get bot info
 var me = await tg.GetMeAsync(new GetMeRequestParams(), CancellationToken.None);
