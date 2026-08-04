07a8056fd0172e0e5e9549cf1a0c3d494057231e
thehuginn <anton.stafeyev@gmail.com>
2026-08-04 17:06:27 +0500

    reorg

--- Telegram.cs
+++ Telegram.cs
@@ -163,6 +163,7 @@ public sealed class Telegram : ITelegramClient
         return await _transport.RequestAsync<bool>(requestParams, _token, cancellationToken);
     }
     
+    /// <inheritdoc />
     public async Task<Message> SendPollAsync(SendPollRequestParams requestParams,
         CancellationToken cancellationToken)
     {
